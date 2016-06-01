6c0d1e162a8b6e7927b9172836aafc4825f11a7a
sterlingnostedt <sterling.nostedt@gmail.com>
2016-06-01 20:05:44 +0000

    all but delete function working, still needs to be implemented

--- app/actions.rb
+++ app/actions.rb
@@ -1,14 +1,49 @@
-# Homepage (Root path)
 get '/' do
   erb :index
 end
 
-get '/contacts' do
-  @contacts = Contact.all.order(first_name: :ASC)
+post '/contacts/new' do
+  content_type :json
+  return_message = {}
+  @contact = Contact.new(first_name: params[:first_name], last_name: params[:last_name], email: params[:email], phone_number: params[:phone_number])
+
+  if @contact.save
+    return_message[:result] = 'success'
+    return_message[:id] = @contact.id
+  else
+    return_message[:result] = 'failed - contact not created'
+  end
+
+  return_message.to_json 
+end
+
+get '/contacts/?' do
+  content_type :json
+  @contacts = Contact.all
   @contacts.to_json
 end
 
+get '/contacts/search' do
+  content_type :json
+  query_term = params[:query].downcase
+  results = Contact.where("lower(first_name) LIKE '%#{query_term}%' OR lower(last_name) LIKE '%#{query_term}%' OR email LIKE '%#{query_term}%'")
+  return results.to_json
+end
+
 get '/contacts/:id' do
-  @contact = Contact.find(params[:id])
+  content_type :json
+  @contact = Contact.find params[:id]
   @contact.to_json
+end
+
+delete '/contacts/:id' do
+  content_type :json
+  return_message = {}
+  @contact = Contact.find params[:id]
+  if @contact.destory
+    return_message[:result] = 'success - contact deleted'
+  else
+    return_message[:result] = 'failed - contact not deleted'
+  end
+  return_message.to_json
 end
--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,4 +1,68 @@
-$(document).ready(function() {
+$(function() {
+  function prepareResultsArea() {
+    $("#results").empty();
+    $("#results").show();
+  }
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  function displayContact(contact) {
+    $("<div class ='well'>").append("<strong>Name: </strong>" + contact.first_name + ' ' + contact.last_name + "<br> <strong>Email: </strong> " + contact.email + "<br> <strong>Phone: </strong> " + contact.phone_number).appendTo("#results")
+  }
+
+  function listContacts() {
+    prepareResultsArea();
+    $("<h3> All Contacts </h3>").appendTo("#results");
+    $.getJSON("/contacts", function(contacts) {
+      $.each(contacts, function(idx, contact) {
+        displayContact(contact);
+      });
+    });
+    }
+
+  function successCreate(data) {
+    prepareResultsArea();
+    $("<p>").append("Contact Created" + data.result ).appendTo("#results");
+  }
+
+  function createContact() {
+    var firstName = $('#first_name').val();
+    var lastName = $('#last_name').val();
+    var email = $('#email').val();
+    var phoneNumber = $('#phone_num').val();
+    $.post( "/contacts/new", {first_name: firstName, last_name: lastName, email: email, phone_number: phoneNumber}, successCreate, "json" )
+  }
+
+  function submitSearch() {
+    prepareResultsArea();
+    $("<h3> Search Results </h3>").appendTo("#results");
+    var query = $('#query').val();
+
+    $.getJSON("/contacts/search?query=" + query, function(contacts) {
+      if (contacts.length == 0) {
+        $("<p> No Records Found </p>").appendTo("#results");
+      } else {
+        $.each(contacts, function(idx, contact) {
+          displayContact(contact);
+        });
+      }
+    }); 
+  }
+
+  function displayControl(){
+    $("#controlArea").children().hide();
+    $("#results").hide();
+
+    if ($(this).data('target') === 'listFunction') {
+      listContacts();
+    } else {
+      $('#' + $(this).data('target')).show();
+    }
+  }
+
+  $('#createContact').on('click', displayControl);
+  $('#searchContacts').on('click', displayControl);
+  $('#listContacts').on('click', displayControl);
+  $('#createSubmit').on('click', createContact);
+  $('#searchSubmit').on('click', submitSearch);
+  $('#displayRecordButton').on('click', getRecord);
+
 });
